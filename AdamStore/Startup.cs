4c2a185c8d8e2f3b97751e14954791e1a5a8efad
apostolis adam <pauladamny@gmail.com>
2018-12-10 09:45:10 -0500

    Add project files.

---
+++ AdamStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdamStore.Startup))]
+namespace AdamStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
